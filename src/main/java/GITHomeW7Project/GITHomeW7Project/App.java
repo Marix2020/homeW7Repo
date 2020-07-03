0a9173af25850b0e14343599e62a7d3b2bec7bde
Marix2020 <maricelocampo888@gmail.com>
2020-07-03 17:56:22 -0400

    HWPractice7

---
+++ src/main/java/GITHomeW7Project/GITHomeW7Project/App.java
@@ -0,0 +1,13 @@
+package GITHomeW7Project.GITHomeW7Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
